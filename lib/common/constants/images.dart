f851f829265bd4093f4c069bc88dd3bd7630aeeb
Kennedy Owusu <kennediowusu@gmail.com>
2025-01-04 03:50:12 +0000

    Refactor image asset paths to organize SVG files and add new progress tracking SVG asset

--- lib/common/constants/images.dart
+++ lib/common/constants/images.dart
@@ -1,10 +1,14 @@
-const String activityTracker = 'assets/images/activity_tracker.svg';
-const String percentages = 'assets/images/percentages.svg';
-const String progressTracking = 'assets/images/progress_tracking.svg';
+// To use the images, import the file and use the image path as a string
 
-const String pageNotFound = 'assets/images/page_not_found.svg';
-const String weightLogo = 'assets/images/weight_logo.svg';
-const String weightLogoPng = 'assets/images/weight_logo.png';
-const String weightLogoWhite = 'assets/images/weight_logo_white.png';
-const String noData = 'assets/images/png/no_data.png';
+// SVG images
+const String activityTracker = 'assets/images/svg/activity_tracker.svg';
+const String percentages = 'assets/images/svg/percentages.svg';
+const String progressTracking = 'assets/images/svg/progress_tracking.svg';
+const String pageNotFound = 'assets/images/svg/page_not_found.svg';
+const String weightLogo = 'assets/images/svg/weight_logo.svg';
 const String noDataSvg = 'assets/images/svg/no_data.svg';
+
+// PNG images
+const String noData = 'assets/images/png/no_data.png';
+const String weightLogoPng = 'assets/images/png/weight_logo.png';
+const String weightLogoWhite = 'assets/images/png/weight_logo_white.png';
